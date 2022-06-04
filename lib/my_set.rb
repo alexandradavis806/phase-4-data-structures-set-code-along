9d45a8b5d7d2452dac1a09737370cb451184b5b8
Alexandra Davis <alex.davis806@gmail.com>
2022-06-04 12:12:47 -0700

    commit tree a83ff4a89484ac095d307aa23be0b60057606f3e

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,10 @@
 # your code here
+
+class mySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end 
+end
